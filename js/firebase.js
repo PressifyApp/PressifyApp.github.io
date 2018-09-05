f93532878f31c0c961ccaedb302a290aaa0bc7d7
Pablo Blanco <pabloblpa@hotmail.com>
2018-09-05 22:36:54 +0200

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Pablo on 26/08/2018.
+ */
+    // Initialize Firebase
+var config = {
+        apiKey: "AIzaSyALvXl7B6wsUl7WfoKzNqcnVMW4ii_C5Go",
+        authDomain: "chip-pabloblancoo.firebaseapp.com",
+        databaseURL: "https://chip-pabloblancoo.firebaseio.com",
+        projectId: "chip-pabloblancoo",
+        storageBucket: "chip-pabloblancoo.appspot.com",
+        messagingSenderId: "930404960141"
+    };
+firebase.initializeApp(config);
